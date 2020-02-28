acd4fd95c04741cd619a8ac6a967b9a22f8983a1
yesntface <i_lekishvili@cu.edu.ge>
2020-02-28 16:01:33 +0400

    header almost finished

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { HeaderGameDataService } from 'src/app/services/header-game-data.service
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   
@@ -13,11 +13,14 @@ export class HeaderComponent implements OnInit {
   balance: number;
 
 
+
   constructor(public data: HeaderGameDataService) { }
 
   ngOnInit(): void {
     this.games = this.data.games;
     this.balance = 10000;
+    document.documentElement.style.setProperty('--color' ,  this.games[0].color);
   }
+
 
 }
