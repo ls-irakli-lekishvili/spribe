41a642a936705aecc185134d380ee3c643be3c56
yesntface <i_lekishvili@cu.edu.ge>
2020-03-01 17:15:59 +0400

    minor bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ProfilePopUpComponent } from './components/profile-pop-up/profile-pop-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfilePopUpComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  exports: [
+    MatDialogModule,
+    ProfilePopUpComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from '../../interfaces/game'
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Game } from '../../interfaces/game';
 import { HeaderGameDataService } from 'src/app/services/header-game-data.service';
+import { ProfilePopUpComponent } from '../profile-pop-up/profile-pop-up.component';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,11 @@ import { HeaderGameDataService } from 'src/app/services/header-game-data.service
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
   games: Game[];
   balance: number;
+  hov: number;
 
-  constructor(public data: HeaderGameDataService) { }
+  constructor(private data: HeaderGameDataService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.games = this.data.games;
@@ -20,5 +22,16 @@ export class HeaderComponent implements OnInit {
     document.documentElement.style.setProperty('--color' ,  this.games[1].color);
   }
 
+  openDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.position = {
+      top: '46px',
+      right: '50px'
+    };
+    dialogConfig.panelClass = 'popup';
+    this.dialog.open(ProfilePopUpComponent, dialogConfig);
+  }
 
 }
